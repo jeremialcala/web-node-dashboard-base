179a36198698fdd28bf695f325af1a4b86f11cc6
Jeremi Alcala <jeremialcala@gmail.com>
2024-07-26 22:32:51 -0400

    cambios en las rutas de los recursos de pantalla de login

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
 })
-
+``
 app.engine('html', require('ejs').renderFile);
 
 app.get('/', (req, res) => {
